8eaca440cbc04e3869fb437fb606ff2f766c78e9
srimanth <113226202+srimanth1234@users.noreply.github.com>
2024-02-13 17:30:36 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Regression testing is primarily related to",
+    answer: "Functional testing",
+  
+    options: [
+      "Maintenance testing",
+      "Data flow testing",
+      "Functional testing",
+      "Development testing"
+    ]
+    
+  },
+    {
+    numb: 2,
+    question: "What is the  worst case time complexity of the Merge sort algorithm",
+    answer: "o(nlogn)",
+    options: [
+      "o(n^2)",
+      "o(nlogn)",
+      "o(n)",
+      "o(logn)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Of the following sort algorithms, which has execution time that is least dependent on initial ordering of the input ?",
+    answer: "Merge sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "Merge sort"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The domain of the function log( log sin(x) ) is",
+    answer: "Empty set",
+    options: [
+      "0 < x < π",
+      "Empty set",
+      "0 < x < π",
+      "2nπ < x < (2n + 1) π , for n in N "
+    ]
+  },
+    
+];
+
